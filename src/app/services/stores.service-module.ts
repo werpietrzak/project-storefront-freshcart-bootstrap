ce79e869d588f801946bd5c4edde0b798864c751
Wer Pietrzak <95078050+werpietrzak@users.noreply.github.com>
2023-01-24 18:49:57 +0100

    feat: create stores service

---
+++ src/app/services/stores.service-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { StoresService } from './stores.service';
+
+@NgModule({
+  imports: [],
+  declarations: [],
+  providers: [StoresService],
+  exports: []
+})
+export class StoresServiceModule {
+}
