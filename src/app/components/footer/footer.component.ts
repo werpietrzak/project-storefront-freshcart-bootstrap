379948c34d259a6e8caa84a46175f4e177d9ca1f
Wer Pietrzak <95078050+werpietrzak@users.noreply.github.com>
2023-01-24 19:19:06 +0100

    feat: create footer component

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,24 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { CategoriesService } from "../../services/categories.service";
+import { Observable } from "rxjs";
+import { CategoryModel } from "../../models/category.model";
+import { StoresService } from "../../services/stores.service";
+import { StoreModel } from "../../models/store.model";
+
+@Component({
+  selector: 'app-footer',
+  styleUrls: ['./footer.component.scss'],
+  templateUrl: './footer.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FooterComponent {
+  readonly categories$: Observable<CategoryModel[]> = this._categoriesService.getCategories();
+
+  readonly stores$: Observable<StoreModel[]> = this._storesService.getStores();
+
+  constructor(
+    private _categoriesService: CategoriesService,
+    private _storesService: StoresService,
+  ) {}
+}
