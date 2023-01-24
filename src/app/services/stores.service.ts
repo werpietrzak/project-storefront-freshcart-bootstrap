8c4e12fca2026bac7408fdeb360d980f05b20e07
Wer Pietrzak <95078050+werpietrzak@users.noreply.github.com>
2023-01-24 18:49:57 +0100

    feat: create stores service

---
+++ src/app/services/stores.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { StoreModel } from "../models/store.model";
+
+@Injectable()
+export class StoresService {
+  constructor(private _httpClient: HttpClient) {}
+
+  getStores(): Observable<StoreModel[]> {
+    return this._httpClient.get<StoreModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-stores');
+  }
+}
