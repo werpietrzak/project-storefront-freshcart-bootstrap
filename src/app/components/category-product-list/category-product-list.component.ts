d82164948774f1d20452e137d859cee3bc14f167
Wer Pietrzak <95078050+werpietrzak@users.noreply.github.com>
2023-01-28 12:21:48 +0100

    feat: add product list

--- src/app/components/category-product-list/category-product-list.component.ts
+++ src/app/components/category-product-list/category-product-list.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { Observable, switchMap } from "rxjs";
+import { combineLatest, map, Observable, switchMap } from "rxjs";
 import { CategoryModel } from "../../models/category.model";
 import { CategoriesService } from "../../services/categories.service";
 import { ActivatedRoute } from "@angular/router";
+import { ProductsService } from "../../services/products.service";
+import { ProductQueryModel } from "../../queryModels/product-query.model";
 
 @Component({
   selector: 'app-category-product-list',
@@ -18,8 +20,41 @@ export class CategoryProductListComponent {
     switchMap(params => this._categoriesService.getOneCategory(params['categoryId']))
   );
 
+  readonly products$: Observable<ProductQueryModel[]> = combineLatest([
+    this.selectedCategory$,
+    this._productsService.getAllProducts(),
+  ]).pipe(
+    map(([category, products]) => products.reduce((acc: ProductQueryModel[], cur) => (
+      cur.categoryId === category.id ? [...acc, {
+        name: cur.name,
+        price: cur.price,
+        category: category.name,
+        ratingValue: cur.ratingValue,
+        ratingCount: cur.ratingCount,
+        ratingStars: this.convertRatingToStars(cur.ratingValue),
+        imageUrl: cur.imageUrl,
+        id: cur.id,
+      }] : acc), [])
+    )
+  );
+
   constructor(
     private _categoriesService: CategoriesService,
+    private _productsService: ProductsService,
     private _activatedRoute: ActivatedRoute,
     ) {}
+
+  convertRatingToStars(rating: number): number[] {
+    let result = new Array(5).fill(0);
+    for (let i = 0; i < 5; i++) {
+      if (rating >= 1) {
+        result[i] = 1;
+        rating -= 1;
+      } else {
+        result[i] = rating;
+        break;
+      }
+    }
+    return result;
+  }
 }
---
+++ src/app/models/product.model.ts
@@ -0,0 +1,11 @@
+export interface ProductModel {
+  name: string;
+  price: number;
+  categoryId: string;
+  ratingValue: number;
+  ratingCount: number;
+  imageUrl: string;
+  featureValue: number;
+  storeIds: string[];
+  id: string;
+}
