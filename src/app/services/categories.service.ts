1035b2374ca18ee737cdda3f040b180d79d4b433
Wer Pietrzak <95078050+werpietrzak@users.noreply.github.com>
2023-01-30 13:21:43 +0100

    fix: image paths

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { map, Observable } from "rxjs";
 import { CategoryModel } from "../models/category.model";
 
 @Injectable()
@@ -8,12 +8,27 @@ export class CategoriesService {
   constructor(private _httpClient: HttpClient) {}
 
   getAllCategories(): Observable<CategoryModel[]> {
-    return this._httpClient.get<CategoryModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-categories');
+    return this._httpClient.get<CategoryModel[]>(
+      'https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-categories'
+    ).pipe(
+      map(categories => categories.map(
+        category => ({
+          ...category,
+          imageUrl: category.imageUrl.substring(1),
+        })
+      ))
+    );
   }
 
   getOneCategory(categoryId: string): Observable<CategoryModel> {
     return this._httpClient.get<CategoryModel>(
       `https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-categories/${categoryId}`
+    ).pipe(
+      map(category => ({
+          ...category,
+          imageUrl: category.imageUrl.substring(1),
+        })
+      )
     );
   }
 }
--- src/app/services/stores.service.ts
+++ src/app/services/stores.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { map, Observable } from "rxjs";
 import { StoreModel, StoreTagModel } from "../models/store.model";
 
 @Injectable()
@@ -8,7 +8,15 @@ export class StoresService {
   constructor(private _httpClient: HttpClient) {}
 
   getAllStores(): Observable<StoreModel[]> {
-    return this._httpClient.get<StoreModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-stores');
+    return this._httpClient.get<StoreModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-stores')
+      .pipe(
+        map(stores => stores.map(
+          store => ({
+            ...store,
+            logoUrl: store.logoUrl.substring(1),
+          })
+        ))
+      );
   }
 
   getOneStore(storeId: string): Observable<StoreModel> {
