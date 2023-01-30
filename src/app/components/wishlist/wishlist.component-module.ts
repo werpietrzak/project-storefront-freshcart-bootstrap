91add4292a2281e39f7348ec6dcc96fa3bed705b
Wer Pietrzak <95078050+werpietrzak@users.noreply.github.com>
2023-01-30 09:36:31 +0100

    feat: add remove from wishlist method

--- src/app/components/wishlist/wishlist.component-module.ts
+++ src/app/components/wishlist/wishlist.component-module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { WishlistComponent } from './wishlist.component';
+import { CommonModule } from "@angular/common";
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule],
   declarations: [WishlistComponent],
   providers: [],
   exports: [WishlistComponent]
--- src/app/components/wishlist/wishlist.component.ts
+++ src/app/components/wishlist/wishlist.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {BehaviorSubject, combineLatest, map, Observable} from "rxjs";
+import {ProductModel} from "../../models/product.model";
+import {ProductsService} from "../../services/products.service";
 
 @Component({
   selector: 'app-wishlist',
@@ -8,4 +11,24 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WishlistComponent {
+  private _wishlistedProductsIdsSubject: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(['1', '2', '3']);
+
+  readonly wishlistedProductsIds$: Observable<string[]> = this._wishlistedProductsIdsSubject.asObservable();
+
+  private _products$: Observable<ProductModel[]> = this._productsService.getAllProducts();
+
+  public wishlistedProducts$: Observable<ProductModel[]> = combineLatest([
+    this._products$,
+    this.wishlistedProductsIds$,
+  ]).pipe(
+    map(([products, selectedIds]) => products.filter(product => selectedIds.includes(product.id)))
+  );
+
+  constructor(private _productsService: ProductsService) {}
+
+  removeFromWishlist(productId: string) {
+    this._wishlistedProductsIdsSubject.next(
+      this._wishlistedProductsIdsSubject.value.filter(a => a !== productId)
+    );
+  }
 }
